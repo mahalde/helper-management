f1f88f8231d4d701e69883bd93042e0ddf8f5f33
mahalde <github@malte-thienel.de>
2023-03-21 11:41:49 +0100

    Fix for flash of 500 error

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,15 +1,19 @@
-export const getDateFormatter = (locale?: string | null, withTime = false) => {
-	console.log("DATETIMEFORMATTER:", locale);
-	return new Intl.DateTimeFormat(locale ?? 'de-DE', {
-	month: '2-digit',
-	day: '2-digit',
-	year: 'numeric',
-	hour: withTime ? '2-digit' : undefined,
-	minute: withTime ? '2-digit' : undefined,
-	weekday: 'long'
-})};
+function parseLocaleString(localeString?: string | null) {
+	return localeString?.split(';')[0];
+}
 
-export const getTimeFormatter = (locale?: string | null) => new Intl.DateTimeFormat(locale ?? 'de-DE', {
-	hour: '2-digit',
-	minute: '2-digit'
-});
+export const getDateFormatter = (locale?: string | null, withTime = false) =>
+	new Intl.DateTimeFormat(parseLocaleString(locale) ?? 'de-DE', {
+		month: '2-digit',
+		day: '2-digit',
+		year: 'numeric',
+		hour: withTime ? '2-digit' : undefined,
+		minute: withTime ? '2-digit' : undefined,
+		weekday: 'long'
+	});
+
+export const getTimeFormatter = (locale?: string | null) =>
+	new Intl.DateTimeFormat(parseLocaleString(locale) ?? 'de-DE', {
+		hour: '2-digit',
+		minute: '2-digit'
+	});
