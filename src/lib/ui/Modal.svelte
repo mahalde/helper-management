0d7789db0195ba05a25f124748d5611161e01dd6
mahalde <github@malte-thienel.de>
2023-03-27 16:40:52 +0200

    Added filters to dashboard

--- src/lib/ui/Modal.svelte
+++ src/lib/ui/Modal.svelte
@@ -18,7 +18,7 @@
 
 <div bind:this={modalEl} aria-hidden="true" class="hidden fixed inset-0 z-10 overflow-y-auto">
 	<div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-	<div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+	<div class="flex min-h-full justify-center p-4 text-center items-center sm:p-0">
 		<div
 			class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
 		>
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,14 +1,37 @@
 <script lang="ts">
-	import { PERMISSIONS, type Slot, type SortOption } from '$lib/types';
+	import {
+		PERMISSIONS,
+		SlotCategory,
+		type Modal as ModalType,
+		type Slot,
+		type SortOption
+	} from '$lib/types';
 	import Icon from '$lib/ui/Icon.svelte';
-	import { adjustments_horizontal } from '$lib/ui/icons';
+	import { adjustments_horizontal, funnel } from '$lib/ui/icons';
+	import Modal from '$lib/ui/Modal.svelte';
 	import SlotDisplay from '$lib/ui/SlotDisplay.svelte';
-	import { _ } from 'svelte-i18n';
+	import { getDateFormatter } from '$lib/utils';
+	import { locale, _ } from 'svelte-i18n';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 
 	let yourSlots: Slot[] = [];
+	let filteredSlots: Slot[] = [];
+
+	const slotMetadata = {
+		dates: new Set<string>(),
+		categories: new Set<SlotCategory>(),
+		names: new Set<string>()
+	};
+	let filters = {
+		dates: new Set<string>(),
+		categories: new Set<SlotCategory>(),
+		names: new Set<string>()
+	};
+	let filtered = false;
+
+	const dateFormatter = getDateFormatter($locale);
 
 	const sortOptions: SortOption[] = [
 		{ type: 'date', sort: 'asc', label: 'label.date.asc' },
@@ -18,10 +41,53 @@
 	];
 	let selectedSortOption = sortOptions[0];
 
-	$: yourSlots =
-		data.slots?.filter((slot) =>
-			slot.helpers.some((helper) => helper.id === data.session?.user.id)
-		) ?? [];
+	$: {
+		data.slots?.forEach((slot) => {
+			slotMetadata.dates.add(slot.start_time.toISOString().split('T')[0]);
+			slotMetadata.categories.add(slot.category);
+			slotMetadata.names.add(slot.name);
+		});
+
+		slotMetadata.names = new Set([...slotMetadata.names].sort())
+
+		yourSlots =
+			data.slots?.filter((slot) =>
+				slot.helpers.some((helper) => helper.id === data.session?.user.id)
+			) ?? [];
+	}
+
+	$: {
+		if (!filtered) {
+			filters.dates = new Set(slotMetadata.dates);
+			filters.categories = new Set(slotMetadata.categories);
+			filters.names = new Set(slotMetadata.names);
+		}
+
+		filteredSlots =
+			data.slots?.filter((slot) =>
+				filters.dates.has(slot.start_time.toISOString().split('T')[0])
+				&& filters.categories.has(slot.category)
+				&& filters.names.has(slot.name)
+			) ?? [];
+	}
+
+	let filterModal: ModalType;
+
+	function filterSlots(event: SubmitEvent) {
+		const formData = new FormData(event.target as HTMLFormElement);
+
+		const dates = formData.getAll('dates') as string[];
+		filters.dates = new Set(dates);
+
+		const categories = formData.getAll('categories') as SlotCategory[];
+		filters.categories = new Set(categories);
+
+		const names = formData.getAll('names') as string[];
+		filters.names = new Set(names);
+
+		filtered = true;
+		filterModal.hide();
+	}
 </script>
 
 <div class="my-4 w-fit mx-2 sm:mx-auto">
@@ -58,11 +124,75 @@
 					{/each}
 				</select>
 			</label>
+			<button
+				type="button"
+				on:click={filterModal.show}
+				class="btn btn-sm variant-ringed-secondary w-fit"
+			>
+				<Icon icon={funnel} viewBoxHeight={24} viewBoxWidth={24} />
+				<span>{$_('page.dashboard.show_filter')}</span>
+			</button>
 		{/if}
 	</div>
-	{#if data.slots?.length}
-		<SlotDisplay slots={data.slots} sortOption={selectedSortOption} />
+	{#if filteredSlots?.length}
+		<SlotDisplay slots={filteredSlots} sortOption={selectedSortOption} />
 	{:else}
 		<p class="dark:text-white">{$_('page.dashboard.no_times')}</p>
 	{/if}
 </div>
+
+<Modal bind:modal={filterModal}>
+	<form on:submit|preventDefault={filterSlots} class="p-4 space-y-2 flex flex-col">
+		<p class="unstyled text-xl">{$_('label.date')}</p>
+		<div>
+			{#each [...slotMetadata.dates] as date (date)}
+				<label class="flex items-center gap-2">
+					<input
+						type="checkbox"
+						name="dates"
+						value={date}
+						class="checkbox variant-ringed-primary"
+						checked={filters.dates.has(date)}
+					/>
+					<p>{dateFormatter.format(new Date(date))}</p>
+				</label>
+			{/each}
+		</div>
+		<hr />
+		<p class="unstyled text-xl">{$_('label.category')}</p>
+		<div>
+			{#each [...slotMetadata.categories] as category (category)}
+				<label class="flex items-center gap-2">
+					<input
+						type="checkbox"
+						name="categories"
+						value={category}
+						class="checkbox variant-ringed-primary"
+						checked={filters.categories.has(category)}
+					/>
+					<p>{$_(`label.${category}`)}</p>
+				</label>
+			{/each}
+		</div>
+		<hr />
+		<p class="unstyled text-xl">{$_('label.name')}</p>
+		<div>
+			{#each [...slotMetadata.names] as name (name)}
+				<label class="flex items-center gap-2">
+					<input
+						type="checkbox"
+						name="names"
+						value={name}
+						class="checkbox variant-ringed-primary"
+						checked={filters.names.has(name)}
+					/>
+					<p>{name}</p>
+				</label>
+			{/each}
+		</div>
+		<button type="submit" class="btn btn-sm variant-filled-primary">
+			<Icon icon={funnel} viewBoxHeight={24} viewBoxWidth={24} />
+			<span>{$_('page.dashboard.apply_filter')}</span>
+		</button>
+	</form>
+</Modal>
